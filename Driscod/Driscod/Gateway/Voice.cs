3cae4f2c334b9dc29626b61bcd8faf120cabda56
lewisc64 <lewisbcarpenter_@outlook.com>
2020-11-06 04:22:41 +0000

    voice external address discovery

--- Driscod/Driscod/Gateway/Voice.cs
+++ Driscod/Driscod/Gateway/Voice.cs
@@ -1,5 +1,13 @@
-﻿using MongoDB.Bson;
+﻿using Concentus.Enums;
+using Concentus.Structs;
+using MongoDB.Bson;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 
 namespace Driscod.Gateway
 {
@@ -15,6 +23,8 @@ namespace Driscod.Gateway
 
         private readonly string _token;
 
+        private UdpClient _udpClient = null;
+
         private BsonDocument Identity => new BsonDocument
         {
             { "server_id", _serverId },
@@ -23,7 +33,30 @@ namespace Driscod.Gateway
             { "token", _token },
         };
 
-        public override string Name => "VOICE";
+        private string UdpSocketIpAddress { get; set; }
+
+        private ushort UdpSocketPort { get; set; }
+
+        private UdpClient UdpClient
+        {
+            get
+            {
+                if (_udpClient == null)
+                {
+                    _udpClient = new UdpClient();
+                    _udpClient.Connect(UdpSocketIpAddress, UdpSocketPort);
+                }
+                return _udpClient;
+            }
+        }
+
+        private IEnumerable<string> Modes { get; set; }
+
+        private string Mode => "xsalsa20_poly1305_suffix";
+
+        private uint Ssrc { get; set; }
+
+        public override string Name => $"VOICE-{_sessionId}";
 
         public Voice(string url, string serverId, string userId, string sessionId, string token)
             : base(url)
@@ -35,12 +68,54 @@ namespace Driscod.Gateway
 
             Socket.Opened += (a, b) =>
             {
-                Send((int)MessageType.Identify, Identity);
+                if (KeepSocketOpen)
+                {
+                    Send((int)MessageType.Resume, Identity);
+                }
+                else
+                {
+                    Send((int)MessageType.Identify, Identity);
+                }
             };
 
             AddListener((int)MessageType.Ready, data =>
             {
-                // TODO
+                UdpSocketIpAddress = data["ip"].AsString;
+                UdpSocketPort = (ushort)data["port"].AsInt32;
+                Modes = data["modes"].AsBsonArray.Cast<string>();
+                Ssrc = (uint)data["ssrc"].AsInt32;
+
+                var datagram = new byte[] { 0, 1, 0, 70 }.Concat(BitConverter.GetBytes(Ssrc)).Concat(Enumerable.Repeat((byte)0, 66)).ToArray();
+                UdpClient.Send(datagram, datagram.Length);
+
+                var endpoint = GetUdpEndpoint();
+                var response = UdpClient.Receive(ref endpoint);
+
+                var port = BitConverter.ToUInt16(response, 72);
+
+                var bytes = new List<byte>();
+                foreach (var addrByte in response.Skip(8))
+                {
+                    if (addrByte == 0)
+                    {
+                        break;
+                    }
+                    bytes.Add(addrByte);
+                }
+                var address = Encoding.UTF8.GetString(bytes.ToArray(), 0, bytes.Count);
+
+                Send((int)MessageType.SelectProtocol, new BsonDocument
+                {
+                    { "protocol", "udp" },
+                    { "data",
+                        new BsonDocument
+                        {
+                            { "address", address },
+                            { "port", port },
+                            { "mode", Mode },
+                        }
+                    },
+                });
             });
 
             AddListener((int)MessageType.Hello, data =>
@@ -58,6 +133,54 @@ namespace Driscod.Gateway
         protected override void Heartbeat()
         {
             Send((int)MessageType.Heartbeat, _random.Next(int.MinValue, int.MaxValue));
+        }
+
+        private void IndicateSpeaking(Action callback)
+        {
+            Send((int)MessageType.Speaking, new BsonDocument
+            {
+                { "ssrc", Ssrc },
+                { "delay", 0 },
+                { "speaking", 1 },
+            });
+
+            try
+            {
+                callback();
+            }
+            finally
+            {
+                Send((int)MessageType.Speaking, new BsonDocument
+                {
+                    { "ssrc", Ssrc },
+                    { "delay", 0 },
+                    { "speaking", 0 },
+                });
+            }
+        }
+
+        //private void SendAudio(short[] samples)
+        //{
+        //    OpusEncoder encoder = OpusEncoder.Create(48000, 2, OpusApplication.OPUS_APPLICATION_AUDIO);
+
+        //    ushort sequence = 0;
+        //    int frameSize = 960;
+
+        //    for (var i = 0; i < samples.Length; i += frameSize)
+        //    {
+        //        var inputBuffer = samples.Skip(i).Take(frameSize).ToArray();
+        //        var outputBuffer = new byte[1000];
+        //        int packetSize = encoder.Encode(inputBuffer, 0, frameSize, outputBuffer, 0, outputBuffer.Length);
+
+        //        var timestamp = DateTime.UtcNow.
+
+        //        sequence++;
+        //    }
+        //}
+
+        private IPEndPoint GetUdpEndpoint()
+        {
+            return new IPEndPoint(new IPAddress(UdpSocketIpAddress.Split(new[] { '.' }).Select(x => Convert.ToByte(x)).ToArray()), UdpSocketPort);
         }
 
         public enum MessageType
